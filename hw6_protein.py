725b957100849d4770a1cd72116f071eacb6c2db
Deekshapulluru <deekshapulluru1@msitprogram.net>
2022-04-19 09:54:21 +0530

    Convert DNA to RNA Done

--- hw6_protein.py
+++ hw6_protein.py
@@ -36,9 +36,17 @@ Parameters: str ; int
 Returns: list of strs
 '''
 def dnaToRna(dna, startIndex):
-    return
-
-
+    list=[]
+    n=3
+    Dna=dna.replace("T","U")
+    split_Dna=[Dna[i:i+n] for i in range(startIndex,len(Dna),n)]
+    for i in range(len(split_Dna)):
+        #print(split_Dna[i])
+        list.append(split_Dna[i])
+        if (split_Dna[i]=="UAA" or split_Dna[i]=="UAG" or split_Dna[i]=="UGA"):
+            break
+    #print(list)
+    return list
 '''
 makeCodonDictionary(filename)
 #3 [Check6-1]
